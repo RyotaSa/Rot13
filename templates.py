feb67d56bc79ecac35ca54a3ee13d626eddc334d
RyotaSa <benz.slr.mclaren@gmail.com>
2017-06-19 22:53:42 +0900

    Add files via upload

---
+++ templates.py
@@ -0,0 +1,37 @@
+import os
+
+import jinja2
+import webapp2
+import string
+
+
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja2_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                 autoescape = True)
+
+def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.render('rot13.html')
+    def post(self):
+        rot13 = ''
+        text = self.request.get_all('text')
+        if text:
+            rot13 = text.encode('rot13')
+
+        self.render('rot13.html', text = rot13)
+
+app = webapp2.WSGIApplication([('/rot13', MainPage),], debug = True)
